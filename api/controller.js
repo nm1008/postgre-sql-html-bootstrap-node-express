3f91aead9b6695b053c97c12a9fad6b35063f8dd
nm1008 <mallari088nikko@gmail.com>
2024-03-22 12:46:14 +0800

    implemented on backend (get user by name)

--- api/controller.js
+++ api/controller.js
@@ -17,21 +17,46 @@ const getAllEmployees = (req, res) => {
   }
 };
 
+//get user by name
+const getUserByName = (req, res) => {
+  try {
+    const q =
+      "SELECT * FROM employee_info WHERE first_name = $1 AND last_name = $2;";
+    const { first_name, last_name } = req.body;
+
+    pool.query(q, [first_name, last_name], (err, data) => {
+      if (err) {
+        console.error("Error executing query:", err);
+        return res.status(500).json({ error: "Internal server error" }); // Changed from res.sendStatus(500) to res.status(500).json()
+      }
+      res.status(200).json(data.rows); // Changed from res.sendStatus(200) to res.status(200).json()
+      console.log(data);
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 //add user
 const addEmployeeUser = (req, res) => {
   try {
     const q =
       'INSERT INTO employee_info ("user_id", "first_name", "last_name", "email", "address", "phone_number") VALUES ($1, $2, $3, $4, $5, $6)';
 
-      const { user_id, first_name, last_name, email, address, phone_number } = req.body;
+    const { user_id, first_name, last_name, email, address, phone_number } =
+      req.body;
 
-    pool.query(q, [user_id, first_name, last_name, email, address, phone_number], (err, data) => {
-      if (err) {
-        console.error("Error executing query:", err);
-        return res.sendStatus(500).json({ error: "Internal server error" });
+    pool.query(
+      q,
+      [user_id, first_name, last_name, email, address, phone_number],
+      (err, data) => {
+        if (err) {
+          console.error("Error executing query:", err);
+          return res.sendStatus(500).json({ error: "Internal server error" });
+        }
+        res.status(200).json({ message: "Employee user added successfully." });
       }
-      res.status(200).json({ message: "Employee user added successfully." });
-    });
+    );
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -62,17 +87,22 @@ const updateUserById = (req, res) => {
     const q =
       "UPDATE employee_info SET user_id = $1, first_name = $2, last_name = $3, email = $4, address = $5, phone_number = $6 WHERE user_id = $1";
 
-    const { user_id, first_name, last_name, email, address, phone_number } = req.body;
+    const { user_id, first_name, last_name, email, address, phone_number } =
+      req.body;
 
-    pool.query(q, [user_id, first_name, last_name, email, address, phone_number], (err, data) => {
-      if (err) {
-        console.error("Error executing query:", err);
-        return res.sendStatus(500).json({ error: "Internal server error" });
+    pool.query(
+      q,
+      [user_id, first_name, last_name, email, address, phone_number],
+      (err, data) => {
+        if (err) {
+          console.error("Error executing query:", err);
+          return res.sendStatus(500).json({ error: "Internal server error" });
+        }
+        res
+          .status(200)
+          .json({ message: `Employee ${id} user updated successfully.` });
       }
-      res
-        .status(200)
-        .json({ message: `Employee ${id} user updated successfully.` });
-    });
+    );
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -100,6 +130,7 @@ const deleteUserById = (req, res) => {
 
 module.exports = {
   getAllEmployees,
+  getUserByName,
   addEmployeeUser,
   getUserById,
   updateUserById,
--- api/routes.js
+++ api/routes.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const controller = require("./controller");
 
 //get all employees
-router.get("/", controller.getAllEmployees);
+router.get("/", controller.getAllEmployees, );
 
 //get user by id
 router.get("/:id", controller.getUserById);
+
+//get user by name
+router.post("/search", controller.getUserByName)
 
 //add user
 router.post("/", controller.addEmployeeUser);
