4d408cef7192cf79a7a5947992d56174d1d746bd
nm1008 <mallari088nikko@gmail.com>
2024-03-26 17:17:19 +0800

    for deployment backend

--- api/db.js
+++ api/db.js
@@ -1,11 +1,15 @@
 const { Pool } = require("pg");
 
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "ascent-DB",
+//   password: "root",
+//   port: 5433,
+// });
+
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "ascent-DB",
-  password: "root",
-  port: 5433,
+  connectionString: process.env.POSTGRES_URL,
 });
 
 module.exports = pool;
